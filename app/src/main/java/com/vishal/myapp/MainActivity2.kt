8fec357a939acb6b947b095f244f7ad0f6a37bb2
vishal pandit <121655488+panditvishal04@users.noreply.github.com>
2023-07-06 13:06:03 +0530

    Update MainActivity2.kt

--- app/src/main/java/com/vishal/myapp/MainActivity2.kt
+++ app/src/main/java/com/vishal/myapp/MainActivity2.kt
@@ -15,7 +15,6 @@ class MainActivity2 : AppCompatActivity() {
         tvn=findViewById(R.id.tvn)
         tvo=findViewById(R.id.tvo)
         tvh=findViewById(R.id.tvh)
-//        setActionBar(toolbar)
         val name= intent.getStringExtra("name")
         val age= intent.getStringExtra("age")
         val height= intent.getStringExtra("height")
@@ -23,25 +22,6 @@ class MainActivity2 : AppCompatActivity() {
         tvn.text="Hello  " + name
         tvo.text="you are "+ age +" years old"
         tvh.text="your height is "  + height
-//       val name =intent?.getStringExtra("name")
-//        val age =intent?.getIntExtra("age",0)
-//        val height =intent?.getIntExtra("height",0)
-//        println("name $name age $age height $height")
-////       toolbar = findViewById(R.id.toolbar)
-//        tvn=findViewById(R.id.tvn)
-//        tvo=findViewById(R.id.tvo)
-//        tvh=findViewById(R.id.tvh)
-//        setActionBar(findViewById(R.id.toolbar))
-////        toolbar.title="test"
-//        supportActionBar?.title="test"
-//        tvn.setText("hello $name")
-//        tvo.setText("you are $age years old" )
-//        tvh.setText("your height is $height")
 
-    }
-
-//    private fun setActionBar(toolbar: Toolbar?) {
-//       var toolbar=findViewById<Toolbar>(R.id.toolbar)
-//        toolbar.title="test"
-//    }
+   }
 }
